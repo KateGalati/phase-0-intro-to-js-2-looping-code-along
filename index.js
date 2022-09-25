f4209c98525691b79051882c4ce730c79c2a58e6
Kate Galati <kategalati@Kates-MacBook-Air.local>
2022-09-25 12:07:34 -0700

    commit tree 45ddc6f6c4489eabb8d5c538b33522b314ed4e18

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return messages;
+}
+
+function countDown() {
+    let positiveInt = 10;
+    while (positiveInt >= 0) {
+        console.log(positiveInt--);
+    }
+
+}
